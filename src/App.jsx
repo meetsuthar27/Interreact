24b50e9401d58850e1e7951cd80c1c3ec9c8d5ed
Meet Yashvantbhai Suthar <meetyashvantbhaisuthar@Meets-MacBook-Air-2.local>
2025-02-10 23:34:05 +0530

    Intial Few Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,6 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-=======
-      <h1>Interreact</h1>
-      <h1>Interreact</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
->>>>>>> c072a06 (Intial Changes)
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
